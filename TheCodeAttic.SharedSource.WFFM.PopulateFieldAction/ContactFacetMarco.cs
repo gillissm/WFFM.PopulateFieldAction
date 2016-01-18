4010fe6903b48fbba74e91e7f80137551b6c577a
scott.gillis <scott.gillis@paragon-inc.com>
2016-01-18 16:13:06 -0500

    inital

---
+++ TheCodeAttic.SharedSource.WFFM.PopulateFieldAction/ContactFacetMarco.cs
@@ -0,0 +1,69 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.Rules.RuleMacros;
+using Sitecore.Social.Client.Dialogs.SocialPropertiesSelectors;
+//using Sitecore.Social.Client.Dialogs.SocialPropertiesSelectors;
+//using Sitecore.Social.Client.Rules.DataSources;
+using Sitecore.Text;
+using Sitecore.Web.UI.Sheer;
+using System.Xml.Linq;
+
+namespace TheCodeAttic.SharedSource.WFFM.PopulateFieldAction
+{
+    public class ContactFacetMarco:IRuleMacro
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="element">XML of calling condition rule or action</param>
+        /// <param name="name">property of the condition/action that will be set by the macro</param>
+        /// <param name="parameters">parameter details, the third comma in the text</param>
+        /// <param name="value">currntly set value to the property</param>
+        public void Execute(XElement element, string name, UrlString parameters, string value)
+        {
+            Assert.ArgumentNotNull((object)element, "element");
+            Assert.ArgumentNotNull((object)name, "name");
+            Assert.ArgumentNotNull((object)parameters, "parameters");
+            Assert.ArgumentNotNull((object)value, "value");
+
+            //SocialPropertiesSelectorOptions propertiesSelectorOptions = new SocialPropertiesSelectorOptions()
+            //{
+            //    Title = Translate.Text("Gender"),
+            //    Text = Translate.Text("Select the gender that you want to use for this rule."),
+            //    Icon = "People/32x32/users2.png",
+            //    DataSourceTypeName = typeof(Sitecore.Social.Client.Rules.DataSources.GenderPropertiesDataSource).FullName
+            //};
+
+
+            //if (!string.IsNullOrEmpty(value))
+            //    propertiesSelectorOptions.SelectedPropertyKey = value;
+            //SheerResponse.ShowModalDialog(propertiesSelectorOptions.ToUrlString().ToString(), true);
+
+
+            /////sitecore/shell/~/xaml/Sitecore.Forms.Shell.UI.Dialogs.UpdateContactDetails.xaml.xml
+
+            //ContactFacetSelectorOptions selectorOptions = new ContactFacetSelectorOptions()
+            //{
+            //    Title = "Contact Facet Picker",
+            //    Text = "Select the contact facet field to be used in the form.",
+            //    Icon = "People/32x32/users2.png",
+            //    SelectedPropertyKey = !string.IsNullOrWhiteSpace(value) ? value : string.Empty
+            //};
+
+            //SheerResponse.ShowModalDialog(selectorOptions.ToUrlString().ToString(), true);
+            UrlString str = new UrlString("/sitecore/shell/~/xaml/TheCodeAttic.SharedSource.WFFM.PopulateFieldAction.Sheer.ContactFacetDialog.aspx");
+            //str["actionid"] = element.Attribute("id").Value;
+            //str["uniqid"] = element.Attribute("uid").Value;
+            //str["id"] = "{2808379D-9284-4060-AC87-BE2BFADF93C6}";//Form
+            
+            SheerResponse.ShowModalDialog(str.ToString(), true);
+
+
+            //https://community.sitecore.net/developers/f/8/t/1201
+
+
+
+
+        }
+    }
+}
---
+++ TheCodeAttic.SharedSource.WFFM.PopulateFieldAction/GetContactFacetValue.cs
@@ -0,0 +1,71 @@
+﻿//STEP 1: Using Statements
+using Sitecore.Analytics;
+using Sitecore.Analytics.Model.Entities;
+using Sitecore.Diagnostics;
+using Sitecore.Forms.Core.Rules;
+using System.Linq;
+
+//STEP 2: Namespace, Class, and Inheritance
+namespace TheCodeAttic.SharedSource.WFFM.PopulateFieldAction
+{
+    public class GetContactFacetValue<T> : ReadValue<T> where T : ConditionalRuleContext
+    {
+        protected override object GetValue()
+        {
+            //STEP 3: Protect ourselves, see Brian Perderson's explaination on Sitecore Asserts - https://briancaos.wordpress.com/2012/01/20/sitecore-diagnostics-assert-statements/
+            Assert.IsNotNull((object)Tracker.Current, "Tracker.Current");
+            Assert.IsNotNull((object)Tracker.Current.Contact, "Tracker.Current.Contact");
+
+            string retValue = string.Empty;
+
+            //STEP 4: Retreive from current contact's facets            
+            switch (this.Name.ToLower())
+            {
+                case "first name":
+                    retValue = Tracker.Current.Contact.GetFacet<IContactPersonalInfo>("Personal").FirstName;
+                    break;
+                case "last name":
+                    retValue = Tracker.Current.Contact.GetFacet<IContactPersonalInfo>("Personal").Surname;
+                    break;
+                case "email":
+                    IContactEmailAddresses ea = Tracker.Current.Contact.GetFacet<IContactEmailAddresses>("Emails");
+                    if (!string.IsNullOrWhiteSpace(ea.Preferred) && ea.Entries.Contains(ea.Preferred))
+                    {
+                        retValue = ea.Entries[ea.Preferred].SmtpAddress;
+                    }
+                    else if (ea.Entries.Keys.Any())
+                    {
+                        retValue = ea.Entries[ea.Entries.Keys.First()].SmtpAddress;
+                    }
+                    break;
+                case "city":
+                    IContactAddresses ca = Tracker.Current.Contact.GetFacet<IContactAddresses>("Addresses");
+                    if (!string.IsNullOrWhiteSpace(ca.Preferred) && ca.Entries.Contains(ca.Preferred))
+                    {
+                        retValue = ca.Entries[ca.Preferred].City;
+                    }
+                    else if (ca.Entries.Keys.Any())
+                    {
+                        retValue = ca.Entries[ca.Entries.Keys.First()].City;
+                    }
+                    break;
+                case "phone":
+                    IContactPhoneNumbers pn = Tracker.Current.Contact.GetFacet<IContactPhoneNumbers>("Phone Numbers");
+                    if (!string.IsNullOrWhiteSpace(pn.Preferred) && pn.Entries.Contains(pn.Preferred))
+                    {
+                        retValue = string.Format("{0}-{1}", pn.Entries[pn.Preferred].CountryCode, pn.Entries[pn.Preferred].Number);
+                    }
+                    else if (pn.Entries.Keys.Any())
+                    {
+                        string firstKey = pn.Entries.Keys.First();
+                        retValue = string.Format("{0}-{1}", pn.Entries[firstKey].CountryCode, pn.Entries[firstKey].Number);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            return (object)retValue;
+        }
+    }
+}
